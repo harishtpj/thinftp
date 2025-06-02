7071bc4cbb4e76b8e170903a55885106cf3c519a
harishtpj <harishtpj@hotmail.com>
2025-06-02 11:38:06 +0530

    Updated README

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # The main CLI entrypoint for thinFTP Server
+#
+# This script parses command-line arguments and starts the thinFTP server.
+
 import argparse
 import getpass
 import sys
@@ -7,7 +10,12 @@ import traceback
 from thinftp.logger import get_logger
 from thinftp.server import start_server
 
-parser = argparse.ArgumentParser(prog="thinftp")
+# Parse command-line arguments
+parser = argparse.ArgumentParser(prog="thinftp",
+                                 description="A simple FTP server for testing purposes",
+                                 epilog="Examples:\n"
+                                        "  thinftp -h localhost -p 2121 -u testuser -d ./testdir\n"
+                                        "  thinftp --bind localhost --port 2121 --user testuser --directory ./testdir")
 
 parser.add_argument('-v', '--version',
                     action="version",
@@ -36,17 +44,27 @@ parser.add_argument('-D', '--debug',
 
 opts = parser.parse_args()
 
+# Get the password from the user
 opts.pswd = getpass.getpass(f"Set Password for {opts.user}: ")
+
+# Get the logger instance
 opts.lgr = get_logger(debug=opts.debug)
+
+# Log the startup message
 opts.lgr.info("Welcome to thinFTP server")
 
 try:
+    # Start the server
     start_server(opts)
 except Exception as e:
+    # Handle any unhandled exceptions
     if opts.debug:
+        # Reraise the exception if --debug is specified
         raise
     else:
+        # Log the exception if --debug is not specified
         tb = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
         opts.lgr.critical("Unhandled exception: \n"+ tb)
+        # Exit with a non-zero status code
         sys.exit(1)
 
